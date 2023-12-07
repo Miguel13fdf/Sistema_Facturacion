c49696cbccd2500d56e53a3a1724ccfaf0b82eed
Miguel13fdf <miguel.haro.est@tecazuay.edu.ec>
2023-12-07 10:22:19 -0500

    comit

--- src/java/modelo/Persona.java
+++ src/java/modelo/Persona.java
@@ -10,5 +10,108 @@ package modelo;
  * @author USUARIO
  */
 public class Persona {
+    private int id_persona;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String celular;
+    private String correo;
+
+    public Persona(int id_persona, String nombre, String apellido, String dni, String celular, String correo) {
+        this.id_persona = id_persona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.celular = celular;
+        this.correo = correo;
+    }
+
+    public Persona() {
+    }
+
+    /**
+     * @return the id_persona
+     */
+    public int getId_persona() {
+        return id_persona;
+    }
+
+    /**
+     * @param id_persona the id_persona to set
+     */
+    public void setId_persona(int id_persona) {
+        this.id_persona = id_persona;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular the celular to set
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
     
 }
